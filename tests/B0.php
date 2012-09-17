aed02f596dafdc37792ee3e84c8bd1b6d02afe9a
Alejandro Carrillo <j.alec.n@gmail.com>
2012-09-17 04:36:07 +0200

    Added new B0 test
    - This "test" will print all documents and its properties on a table
    - Useful and good to use with the other tests

---
+++ tests/B0.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>B1 test php Client</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" media="screen" type="text/css" title="design" href="/tests/design.css" />
+  </head>
+  <body>
+    <?php include 'nav.php' ?>
+    <pre>
+      Execute a <code>SELECT * FROM Document ORDER BY ecm:path</code> query to Nuxeo and print all the document properties. This is to ease testing in the other tests.
+    </pre>
+    <hr />
+<?php
+
+$configuration  = NAPC\Configuration::getInstance();
+$client         = new Nuxeo\PhpAutomationClient($configuration->getUrl(false));
+$session        = $client->getSession($configuration->getUsername(),$configuration->getPassword());
+$answer         = $session->newRequest("Document.Query")
+->set('params', 'query', "SELECT * FROM Document ORDER BY ecm:path")
+->setSchema('*')
+->sendRequest();
+
+$documents = $answer->getDocumentList();
+?>
+    <table>
+      <thead>
+        <tr>
+          <th>uid</th>
+          <th>Path</th>
+          <th>Type</th>
+          <th>State</th>
+          <th>Title</th>
+          <th>Property 1</th>
+          <th>Property 2</th>
+          <th>Download as PDF</th>
+        </tr>
+      </thead>
+      <tbody>
+<?php
+    foreach($documents as $document) {
+?>
+        <tr>
+          <td><pre><?= $document->getUid() ?></pre></td>
+          <td><pre><?= $document->getPath() ?></pre></td>
+          <td><pre><?= $document->getType() ?></pre></td>
+          <td><pre><?= $document->getState() ?></pre></td>
+          <td><pre><?= $document->getTitle() ?></pre></td>
+          <td><pre><?= $document->getProperty('dc:description') ?></pre></td>
+          <td><pre><?= $document->getProperty('dc:creator') ?></pre></td>
+          <td>
+            <form id="test" action="../tests/B5bis.php" method="post" >
+              <input type="hidden" name="data" value="<?= $document->getPath() ?>"/>
+              <input type="submit" value="download"/>
+            </form>
+          </td>
+        </tr>
+<?php
+    }
+?>
+      </tbody>
+    </table>
+  </body>
+</html>
--- tests/nav.php
+++ tests/nav.php
@@ -1,4 +1,5 @@
     <nav>
+      <a href="/B0">B0 test</a> |
       <a href="/B1">B1 test</a> |
       <a href="/B2">B2 test</a> |
       <a href="/B3">B3 test</a> |
